d26e27c1a02fc619564f36db3fe15b0511ff3a96
Marian <mvladoi@ucsc.edu>
2021-11-28 01:25:45 +0100

    Deployed to GCP App Engine

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
 # Kotlin Spring Boot on App Engine Standard
 
-To download and run this sample, download the code with the commands below, and
-then follow the steps in the [Community Tutorial][tutorial].
+
+##Deploy locally 
 
 ```sh
-git clone git@github.com:GoogleCloudPlatform/kotlin-samples
-cd kotlin-samples/appengine/springboot
+git clone https://github.com/mvladoi/sinch-task.git
+cd sinch/springboot
+mvn spring-boot:run 
+
 ```
 
 [tutorial]: https://cloud.google.com/community/tutorials/kotlin-springboot-app-engine-java8
+
+
+## Two Apis 
+1. ```sinch-assignment/all-pairs```
+2. ```sinch-assignment/polish-notation```
+
+
+## Running Time
+1. All pair is the coding exercise : ```Count pairs with given sum```
+   O(n) time
+
+2. Polish notation is the coding exercise: ```Evaluation of Prefix Expressions```
+    O(n) time
+   
+
+## To test it on GCP APP Engine 
+curl -X POST https://fresh-metrics-266911.uc.r.appspot.com/sinch-assignment/all-pairs --data-binary  @sinch/springboot/input_files/all_pairs/file1  -H "Content-Type: application/javascript"
+
+curl -X POST https://fresh-metrics-266911.uc.r.appspot.com/sinch-assignment/polish-notation  --data-binary  @sinch/springboot/input_files/polish_notation/file1  -H "Content-Type: application/javascript"
--- springboot/src/main/kotlin/MessageController.kt
+++ springboot/src/main/kotlin/MessageController.kt
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestBody
 import java.util.*;
 
+
 data class Message(val text: String, val priority: String)
 
 @RestController
@@ -76,7 +77,7 @@ class MessageController {
         }
       
         //return number of pairs 
-        return count.toString()
+        return  "{result: " + count.toString() + "}"
     }
      //check if the file has the corect format
      catch (e: Exception) {
@@ -86,11 +87,23 @@ class MessageController {
 
     }
 
+    //helper function to check if we are dealing with operand
+    fun isOperand (x: String): Boolean {
+        if (x.equals("+") or x.equals("-") or x.equals("*") or x.equals("/") ){ 
+           return false
+        }    
+        else {
+           return true 
+        }
+                
+    }
+
     @RequestMapping(value = ["/sinch-assignment/polish-notation"], method = [RequestMethod.POST])
     fun polish(@RequestBody data: String): String {
         
         //split the input string by new lines, and raise an exception if empty file
         var input: List<String>
+        val res: Vector<String> = Vector<String>()
 
 
         //this is just for safety, curl does not allow empty files
@@ -101,14 +114,67 @@ class MessageController {
         }
         
        
-        for (item in input) {
-             print (item)
+        for (item in input){
+   
+            var stack : Stack<Double> = Stack()
+            //split the expr on space
+            val expr = item.split(" ")
+           
+            //iterate from the end of the expression 
+            for (i in (expr.size -1) downTo 0){
+                //if is opperand push to stack 
+                if (isOperand(expr[i])){
+                     stack.add(expr[i].toDouble())
+                 }
+
+                //else perform the opearation 
+                else {
+                      
+                    //if there are less than 2 operators on stack save error
+                    if (stack.size < 2){
+                        stack.pop()
+                        res.addElement("error")
+                        
+                    } 
+                    
+                    else {
+                        
+                    val op1: Double = stack.peek()
+                    stack.pop()
+                    val op2: Double = stack.peek()
+                    stack.pop()
+                    
+                    when (expr[i]) {
+                        
+                       "+" -> stack.push(op1 + op2)
+                       "-" -> stack.push(op1 - op2)
+                       "*" -> stack.push(op1 * op2)
+                       "/" -> stack.push(op1 / op2)
+                       
+                       
+                    }
+    
+                    
+                   }
+        
+                }
+                  
+                 
+          }
+            //if stack has more than one entries save error               
+            if (stack.size > 1){
+                res.addElement("error")
+            }
+            //if stack has only one entrie save the result 
+            else if (stack.size == 1){
+                res.addElement("%.2f".format(stack.peek())) 
+            }
+           
               
         }    
 
-
-        //return number of pairs 
-        return input.joinToString()
+        //return result  
+        return "{result: " + res.joinToString() + "}"
 
        }
     
@@ -122,76 +188,3 @@ class MessageController {
 
 
 
-
-
-
-import java.util.Stack
-import java.util.Vector
-
-
-fun isOperand (x: String): Boolean {
-    if (x.equals("+") or x.equals("-") or x.equals("*") or x.equals("/") ){ 
-       return false
-    }    
-    else {
-       return true 
-    }
-            
-} 
-
-fun main(args: Array<String>){
-    //print(isOperand(x))
-    
-    
-    val input : List<String> = listOf("+ + 0.5 1.5 * 4 10", "- 2e3 - 700 + 7 * 2 15", "- -1.5 * 3.1415 / -7 -2", "100500", "1 2", "+ 1")
-    val res: Vector<String> = Vector<String>();
-    
-    for (item in input){
-   
-        var stack : Stack<Double> = Stack()
-        //val test: String = input[0]
-        //print (test)
-        val expr = input[0].split(" ")
-        //print (expr.toString())
-        
-        for (i in (expr.size -1) downTo 0){
-             if (isOperand(expr[i])){
-                 stack.add(expr[i].toDouble())
-             }
-             
-              else {
- 
-             
-                val op1: Double = stack.peek()
-                stack.pop()
-                val op2: Double = stack.peek()
-                stack.pop()
-                
-                when (expr[i]) {
-                    
-                   "+" -> stack.push(op1 + op2)
-                   "-" -> stack.push(op1 - op2)
-                   "*" -> stack.push(op1 * op2)
-                   "/" -> stack.push(op1 / op2)
-                   
-                   
-                   else -> { 
-                      print("error")
-                   }
-
-                
-               }
-    
-            }
-              
-             
-      }
-          res.addElement("%.2f".format(stack.peek()))
-          
-    } 
-    
-    for (item in res){
-        print (item)
-    }
-    
-}
